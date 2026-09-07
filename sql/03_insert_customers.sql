e256e6fc42f7b8790075e3420cf7bee85962a72e
Jessica Dudzinski <jessicadudzinski@outlook.com>
2026-09-07 09:52:24 -0500

    Seed customer data

---
+++ sql/03_insert_customers.sql
@@ -0,0 +1,3 @@
+INSERT INTO retail_lab.customers (first_name, last_name, email, phone_number, zip_code, join_date, age, loyalty_status)
+VALUES
+    ('Maya', 'Carter', 'maya.carter@example.com', '4055550123', '73120', '2026-08-05', 34, 'gold');
